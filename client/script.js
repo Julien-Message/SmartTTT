5212a77a737b24f81664e438c14973c73cac54c3
Julien Message <julien.message+git@icloud.com>
2018-05-17 20:58:54 +0200

    add newgame button

--- client/script.js
+++ client/script.js
@@ -2,20 +2,31 @@ let ws = new WebSocket("ws://localhost:5000/game");
 
 const list = document.getElementsByClassName("tile");
 for (let element of list) {
-	element.addEventListener("click", () => {if (!finished) {ws.send(element.id.toString())}});
+	element.addEventListener("click", () => {
+		if (!finished) {
+			console.log(JSON.stringify(element))
+			ws.send(element.id.toString())
+		}
+	});
+}
+
+newGame = function(){
+	console.log("new game");
+	ws.send("new");
+	finished = false;
 }
 
 ws.onopen = function (event) {
-    ws.send("new");
-    finished = false
+    newGame();
 };
 
 ws.onmessage = function (event) {
 	data = JSON.parse(event.data);
-    console.log("message " + data)
+    console.log("message " + JSON.stringify(data))
 	for (i = 0; i < 9; i++) {
 		document.getElementById(i.toString()).innerHTML = ox(data["board"][i])
 	}
+	console.log(data.result)
 	if (data.result == "won") {
 		finished = true
 		document.getElementById("status").innerHTML = "Partie finie"
